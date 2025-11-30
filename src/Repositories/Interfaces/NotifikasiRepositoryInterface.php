3dbbb106944fcbb5f56e02e9382f1de8ed1a38ae
Tesa Firna Ananta <h1101241057@student.untan.ac.id>
2025-11-30 17:31:04 +0700

    Delete src directory

--- src/Repositories/Interfaces/NotifikasiRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Repositories\Interfaces;
-
-interface NotifikasiRepositoryInterface extends RepositoryInterface
-{
-    public function findByPegawaiId(int $pegawaiId): array;
-}
